055fb3975177c0bccabab5cdf76c8f3092503d9d
IAmTheDoctor1977 <ifetogun@gmail.com>
2026-07-19 17:01:53 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'takim-v1';
+const CACHE = 'takim-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
